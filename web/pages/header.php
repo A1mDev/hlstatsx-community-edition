319d65e942308cdf05069d9551951a6aa651a3f1
Rosenstein <Rosenstein@users.noreply.github.com>
2024-07-05 09:28:38 +0200

    fix:  Undef variable $extratabs
    when updater folder was detected

--- web/pages/header.php
+++ web/pages/header.php
@@ -195,7 +195,7 @@ For support and installation notes visit http://www.hlxcommunity.com
 			<ul>
 				<li><a href="<?php echo $g_options['scripturl'] ?>"><img src="<?php echo $iconpath; ?>/title-contents.png" alt="Contents" /></a></li>
 				<li><a href="<?php echo $g_options['scripturl'] ?>?mode=search"><img src="<?php echo $iconpath; ?>/title-search.png" alt="Search" /></a></li>
-				<?php if ($extratabs) { print $extratabs; } ?>				
+				<?php if ($extratabs ?? NULL) { print $extratabs; } ?>				
 				<li><a href="<?php echo $g_options['scripturl'] ?>?mode=help"><img src="<?php echo $iconpath; ?>/title-help.png" alt="Help" /></a></li>
 			</ul>
 
