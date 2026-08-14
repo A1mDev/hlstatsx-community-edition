5f6042d1d97884b4a4c17cc56c8a06207658b55b
A1mDev <33463136+A1mDev@users.noreply.github.com>
2026-08-14 20:38:23 +0700

    Fix "Data too long" errors
    Fix "Data too long" error for columns cli_state, cli_city, and cli_country by increasing their size to 128 in both hlstats_Livestats and hlstats_Players tables.

---
+++ web/updater/80.php
@@ -0,0 +1,21 @@
+<?php
+    if ( !defined('IN_UPDATER') )
+    {
+        die('Do not access this file directly.');
+    }
+
+    $dbversion = 80;
+
+    // Perform database schema update notification
+    print "Updating database and verion schema numbers.<br />";
+
+    $db->query("UPDATE hlstats_Options SET `value` = '$dbversion' WHERE `keyname` = 'dbversion'");
+
+    $db->query("ALTER TABLE `hlstats_Livestats` MODIFY `cli_state` VARCHAR(128)");
+    $db->query("ALTER TABLE `hlstats_Livestats` MODIFY `cli_country` VARCHAR(128)");
+    $db->query("ALTER TABLE `hlstats_Livestats` MODIFY `cli_city` VARCHAR(128)");
+
+    $db->query("ALTER TABLE `hlstats_Players` MODIFY `country` VARCHAR(128)");
+    $db->query("ALTER TABLE `hlstats_Players` MODIFY `state` VARCHAR(128)");
+    $db->query("ALTER TABLE `hlstats_Players` MODIFY `city` VARCHAR(128)");
+?>
